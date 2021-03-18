60ce999968ac9b88fb459f85910c67a88632cf90
unknown <gangishettymanish9@gmail.com>
2021-03-18 10:58:43 +0530

    a

--- first_project/V1.1.3__first_script.sql
+++ first_project/V1.1.3__first_script.sql
@@ -1,6 +1,10 @@
-CREATE TABLE School
+
+USE SCHEMA PUBLIC;
+CREATE TABLE Temp
 (
    FIRST_NAME STRING,
    LAST_NAME STRING,
    Mobile STRING
 );
+
+insert into temp VALUES("manish","kumar","234");
