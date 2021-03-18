f6057e1fafbb6079edef787f96943957f323015f
Manish Kumar <32029289+manumanish@users.noreply.github.com>
2021-03-18 11:23:23 +0530

    Create V1.1.4__sci.sql

---
+++ first_project/V1.1.4__sci.sql
@@ -0,0 +1,9 @@
+CREATE TABLE EMP_DET
+(
+   FIRST_NAME STRING,
+   LAST_NAME STRING,
+);
+
+insert into EMP_DET(FIRST_NAME,LAST_NAME) VALUES ('manish','kumar');
+insert into EMP_DET(FIRST_NAME,LAST_NAME) VALUES ('ravi','kumar');
+insert into EMP_DET(FIRST_NAME,LAST_NAME) VALUES ('ghu','kumar');
